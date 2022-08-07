9473b9422d5327ac92a2ac766b215a1ccc35ebeb
tomasguell <tomasguell123@gmail.com>
2022-08-07 18:29:22 -0300

    Tabla Ventas Implementada y Relacionada

--- SistemaClientes/app.py
+++ SistemaClientes/app.py
@@ -1,5 +1,5 @@
 import database
-
+import datetime
 MENU_PROMPT = """------ MENU ------
 
 Elija una opción:
@@ -8,7 +8,10 @@ Elija una opción:
 2. Mostrar clientes
 3. Buscar cliente
 4. Buscar cliente por DNI
-5. Salir
+5. Agregar venta
+6. Mostrar ventas por cliente
+7. Mostrar todas las ventas
+8. Salir
 
 Su eleccion:"""
 
@@ -16,7 +19,7 @@ def menu():
     connection =database.connect()
     database.createTables(connection)
     
-    while (user_input := input(MENU_PROMPT)) != "5":
+    while (user_input := input(MENU_PROMPT)) != "8":
         if user_input == '1':
             prompt_add_client(connection)
         elif user_input == '2':
@@ -25,23 +28,29 @@ def menu():
             prompt_search_client_by_name(connection)     
         elif user_input == '4':
             prompt_search_client_by_dni(connection)           
+        elif user_input == '5':
+            prompt_add_venta(connection)      
+        elif user_input == '6':
+            prompt_get_ventas_by_cliente(connection)   
+        elif user_input == '7':
+            prompt_get_ventas_data_clientes(connection)                   
         else:
             print("Opción inválida")
 
-
+#opcion 1
 def prompt_add_client(connection):
     nombre = input("Nombre: ")
     apellido = input("Apellido: ")
-    telefono = input("Telefono: ")
-    direccion = input("Direccion: ")
+    telefono = input("(Opcional) Telefono: ")
+    direccion = input("(Opcional) Direccion: ")
     dni = int(input("DNI: "))
     database.addClient(connection, nombre, apellido, telefono, direccion,dni)    
-    
+#opcion 2    
 def prompt_get_clients(connection):
     clients = database.getClients(connection)
     for client in clients:
         print(client)
-                
+#opcion 3                
 def prompt_search_client_by_name(connection):
     nombre = input("Nombre: ")
     clients = database.getClientsByName(connection, nombre)
@@ -50,7 +59,7 @@ def prompt_search_client_by_name(connection):
     else:    
         for client in clients:
             print(client)    
-
+#opcion 4
 def prompt_search_client_by_dni(connection):
     dni = input("DNI: ")
     clients = database.getClientsByDni(connection, dni)
@@ -59,4 +68,46 @@ def prompt_search_client_by_dni(connection):
     else:    
         for client in clients:
             print(client)        
+      
+#opcion 5            
+def prompt_add_venta(connection):
+    fecha = input("Fecha dd/mm/aaaa (INGRESE H SI QUIERE INGRESAR LA FECHA ACTUAL): ")
+    if fecha.lower() == 'h':
+        fecha=datetime.datetime.now().strftime("%d/%m/%Y")
+    monto_venta = float(input("Monto de la venta: "))
+    while True:
+        cliente_id = int(input("ID del cliente: "))
+        try:
+            database.addVenta(connection, fecha, monto_venta, cliente_id)
+            break
+        except Exception:
+            print('El cliente no existe')
+#opcion 6
+def prompt_get_ventas(connection):
+    ventas = database.getVentas(connection)
+    if len(ventas) == 0:    
+        print('No se encontró ninguna venta')
+    else:    
+        for venta in ventas:
+            print(venta)
+#opcion 7       
+def prompt_get_ventas_data_clientes(connection):
+    ventas = database.getClientDataVenta(connection)
+    if len(ventas) == 0:    
+        print('No se encontró ninguna venta')
+    else:    
+        for venta in ventas:
+            print(venta)
+                
+      
+def prompt_get_ventas_by_cliente(connection):
+    cliente_id = input("ID del cliente: ")
+    ventas = database.getVentasByClient(connection, cliente_id)
+    if len(ventas) == 0:    
+        print('No se encontró ninguna venta realizada al cliente')
+    else:    
+        for venta in ventas:
+            print(venta)      
+      
+            
 menu()    
--- SistemaClientes/database.py
+++ SistemaClientes/database.py
@@ -1,27 +1,45 @@
 import sqlite3 as sql
 def connect():
     return sql.connect('database.db')
-
+    
 
 
 CREATE_CLIENT_TABLE = "CREATE TABLE if not exists Clientes (id integer primary key, nombre text, apellido text,telefono text DEFAULT NULL, direccion text DEFAULT NULL,dni integer);"
+CREATE_VENTAS_TABLE = "CREATE TABLE if not exists Ventas (id integer primary key,fecha date, monto_venta float, cliente_id integer,foreign key (cliente_id) references Clientes(id));"
 
 INSERT_CLIENT = "INSERT INTO Clientes (nombre, apellido, telefono, direccion, dni) VALUES (?,?,?,?,?);"
+INSERT_VENTA = "INSERT INTO Ventas (fecha, monto_venta, cliente_id) VALUES (?,?,?);"
 
 GET_CLIENTS = "SELECT * FROM Clientes;"
+GET_VENTAS = "SELECT * FROM Ventas;"
 
 GET_CLIENTS_BY_NAME = "SELECT * FROM Clientes WHERE nombre = ?;"
-
 GET_CLIENTS_BY_DNI = "SELECT * FROM Clientes WHERE dni = ?;"
+GET_CLIENT_DATA_VENTA = "SELECT Ventas.cliente_id, Clientes.nombre, Clientes.apellido, Clientes.dni,Ventas.fecha, Ventas.monto_venta FROM Clientes INNER JOIN Ventas ON Clientes.id = Ventas.cliente_id;"
+GET_VENTAS_BY_CLIENT = "SELECT Clientes.nombre, Clientes.apellido, Clientes.dni,Ventas.fecha, Ventas.monto_venta FROM Clientes INNER JOIN Ventas ON Clientes.id = Ventas.cliente_id WHERE Clientes.id = ?;"
+
 def createTables(connection):  
 
     with connection:
+        connection.execute("PRAGMA foreign_keys = ON;")
         connection.execute(CREATE_CLIENT_TABLE)
+        connection.execute(CREATE_VENTAS_TABLE)
+        
         
 def addClient(connection, nombre, apellido, telefono, direccion,dni):
     with connection:
         connection.execute(INSERT_CLIENT, (nombre, apellido, telefono, direccion,dni))    
+
         
+def addVenta(connection, fecha, monto_venta, cliente_id):
+    with connection:
+        connection.execute(INSERT_VENTA, (fecha, monto_venta, cliente_id))
+
+def getVentas(connection):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(GET_VENTAS)
+        return cursor.fetchall()
 
 def getClients(connection):
     with connection:
@@ -41,3 +59,15 @@ def getClientsByDni(connection, dni):
         cursor.execute(GET_CLIENTS_BY_DNI, (dni,))
         return cursor.fetchall()    
                 
+def getClientDataVenta(connection):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(GET_CLIENT_DATA_VENTA)
+        return cursor.fetchall()                
+    
+    
+def getVentasByClient(connection, Id):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(GET_VENTAS_BY_CLIENT, (Id,))
+        return cursor.fetchall()    
